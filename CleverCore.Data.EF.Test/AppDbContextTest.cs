414f561d22aee86fbd45c3477bc3288cf71923ba
AnhTT <kimcuongden1712@gmail.com>
2023-05-05 07:36:41 +0700

    [UX-4] int commit

---
+++ CleverCore.Data.EF.Test/AppDbContextTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace CleverCore.Data.EF.Test
+{
+    public class AppDbContextTest
+    {
+        [Fact]
+        public void Constructor_CreateInMemoryDb_Success()
+        {
+            var context = ContextFactorycs.Create();
+            Assert.True(context.Database.EnsureCreated());
+        }
+    }
+}
