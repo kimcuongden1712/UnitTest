de70e2b07462af133d3304a7693807bbf4988192
AnhTT <kimcuongden1712@gmail.com>
2023-05-07 01:46:26 +0700

    [UX-4] Mock DB Initial, EF Repository, UnitOfWork

---
+++ CleverCore.Data.EF.Test/EFRepositoryTest.cs
@@ -0,0 +1,113 @@
+﻿using CleverCore.Data.Entities;
+using CleverCore.Infrastructure.Interfaces;
+using System.Linq;
+using Xunit;
+
+namespace CleverCore.Data.EF.Test
+{
+    public class EFRepositoryTest
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EFRepositoryTest()
+        {
+            _appDbContext = ContextFactory.Create();
+            _appDbContext.Database.EnsureCreated();
+            _unitOfWork = new EFUnitOfWork(_appDbContext);
+        }
+
+        [Fact]
+        public void ConstructorShouldSuccessWhenCreateEfRepository()
+        {
+            EfRepository<Function, string> repository = new EfRepository<Function, string>(_appDbContext);
+            Assert.NotNull(repository);
+        }
+
+        [Fact]
+        public void Add_InsertNewRecord_Success()
+        {
+            EfRepository<Function, string> efRepository = new EfRepository<Function, string>(_appDbContext);
+            efRepository.Add(new Function()
+            {
+                Id = "USER",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 1,
+                IconCss = ""
+            });
+            _unitOfWork.Commit();
+            Function function = efRepository.FindById("USER");
+            Assert.NotNull(function);
+        }
+
+        [Fact]
+        public void Add_FindAll_Success()
+        {
+            EfRepository<Function, string> efRepository = new EfRepository<Function, string>(_appDbContext);
+            efRepository.Add(
+            new Function()
+            {
+                Id = "USER",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 1,
+                IconCss = ""
+            });
+            efRepository.Add(
+            new Function()
+            {
+                Id = "ROLE",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 2,
+                IconCss = ""
+            });
+            efRepository.Add(
+            new Function()
+            {
+                Id = "EX",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 1,
+                IconCss = ""
+            });
+            _unitOfWork.Commit();
+            var functions = efRepository.FindAll().ToList();
+            Assert.Equal(3, functions.Count);
+        }
+
+        [Fact]
+        public void Add_FindById_Success()
+        {
+            EfRepository<Function, string> efRepository = new EfRepository<Function, string>(_appDbContext);
+            efRepository.Add(
+            new Function()
+            {
+                Id = "USER",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 1,
+                IconCss = ""
+            });
+            efRepository.Add(
+            new Function()
+            {
+                Id = "ROLE",
+                Name = "test",
+                Status = Enums.Status.Active,
+                URL = "/test",
+                SortOrder = 2,
+                IconCss = ""
+            });
+            _unitOfWork.Commit();
+            var functions = efRepository.FindById("USER");
+            Assert.Equal("USER", functions.Id);
+        }
+    }
+}
---
+++ CleverCore.Data.EF.Test/EFUnitOfWorkTest.cs
@@ -0,0 +1,39 @@
+﻿using CleverCore.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleverCore.Data.EF.Test
+{
+    public class EFUnitOfWorkTest
+    {
+        private readonly AppDbContext _appDbContext;
+        public EFUnitOfWorkTest()
+        {
+            _appDbContext = ContextFactory.Create();
+        }
+
+        [Fact]
+        public void Commit_Success()
+        {
+            var efRepository = new EfRepository<Function, string>(_appDbContext);
+            var unitOfWork = new EFUnitOfWork(_appDbContext);
+            efRepository.Add(
+                new Function()
+                {
+                    Id = "USER",
+                    Name = "test",
+                    Status = Enums.Status.Active,
+                    URL = "/test",
+                    SortOrder = 1,
+                    IconCss = ""
+                });
+            unitOfWork.Commit();
+            var functions = efRepository.FindAll().ToList();
+            Assert.Single(functions);
+        }
+    }
+}
